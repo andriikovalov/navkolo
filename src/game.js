95c5c55f4f8f80ec468aaad4f77f6dff77052abf
Andrii Kovalov <andrewsk@ukr.net>
2021-06-07 23:59:09 +0200

    added html UI elements

--- src/game.js
+++ src/game.js
@@ -1,9 +1,12 @@
 import Phaser from 'phaser'
-import leftArrow from './assets/svg/left_arrow.svg'
-import rightArrow from './assets/svg/right_arrow.svg'
-import upArrow from './assets/svg/up_arrow.svg'
-import downArrow from './assets/svg/down_arrow.svg'
-import gear from './assets/svg/gear.svg'
+import leftArrowSvg from './assets/svg/left_arrow.svg'
+import rightArrowSvg from './assets/svg/right_arrow.svg'
+import upArrowSvg from './assets/svg/up_arrow.svg'
+import downArrowSvg from './assets/svg/down_arrow.svg'
+import gearSvg from './assets/svg/gear.svg'
+
+import inputHtml from './assets/html/input.html'
+import messageHtml from './assets/html/message.html'
 
 export default class Game extends Phaser.Scene {
   constructor (gameConfig) {
@@ -106,10 +109,12 @@ export default class Game extends Phaser.Scene {
     this.gameState.correctAnswers = {}
 
     /**
-     * Temporary storage for possible next actions for alternative message.
-     * @member {Object.<string, Object>}
+     * Temporary storage for possible next actions for blocking messages.
+     * Outer array corresponds to different buttons on the message, inner array contains
+     * JSON actions to perform after clicking a certain button.
+     * @member {Array.<Array.<Object>>}
      */
-    this.gameState.alternativeNextActions = null
+    this.gameState.nextActions = null
   }
 
   gameWidth () {
@@ -122,6 +127,7 @@ export default class Game extends Phaser.Scene {
 
   create () {
     this.createSvgUi()
+    this.createHtmlUi()
   }
 
   createSvgUi () {
@@ -129,7 +135,6 @@ export default class Game extends Phaser.Scene {
     let svgImageQueued = 5
 
     const addTextureListener = (key) => {
-      console.log(key)
       switch (key) {
         case 'left_arrow':
           this.createArrow(0.0583 * this.gameWidth(), 0.5 * this.gameHeight(), 'left', 'left_arrow')
@@ -153,18 +158,17 @@ export default class Game extends Phaser.Scene {
           break
       }
       if (svgImageQueued === 0) {
-        console.log('TODO: remove')
         this.textures.removeListener('addtexture', addTextureListener)
       }
     }
 
     this.textures.on('addtexture', addTextureListener)
 
-    this.addSvgTexture(leftArrow, 'left_arrow')
-    this.addSvgTexture(rightArrow, 'right_arrow')
-    this.addSvgTexture(upArrow, 'up_arrow')
-    this.addSvgTexture(downArrow, 'down_arrow')
-    this.addSvgTexture(gear, 'loading')
+    this.addSvgTexture(leftArrowSvg, 'left_arrow')
+    this.addSvgTexture(rightArrowSvg, 'right_arrow')
+    this.addSvgTexture(upArrowSvg, 'up_arrow')
+    this.addSvgTexture(downArrowSvg, 'down_arrow')
+    this.addSvgTexture(gearSvg, 'loading')
   }
 
   addSvgTexture (svgText, key) {
@@ -184,11 +188,10 @@ export default class Game extends Phaser.Scene {
     arrow.setDisplaySize(arrowSize, arrowSize)
     this.addArrowHover(arrow)
     arrow.on('pointerdown', (pointer, localX, localY, event) => {
-      // TODO object_clicked(phaser_scene, objectName);
-      console.log('click')
+      this.objectClicked(objectName)
       event.stopPropagation()
     })
-    // arrow.setVisible(false);
+    arrow.setVisible(false)
   }
 
   addArrowHover (arrow) {
@@ -208,13 +211,58 @@ export default class Game extends Phaser.Scene {
       .setDepth(1000)
       .setDisplaySize(indicatorSize, indicatorSize)
       .setAlpha(0.7)
-      // .setVisible(false)
+      .setVisible(false)
 
     this.tweens.add({
       targets: this.uiElements.loading,
       angle: 360,
       duration: 5000,
       loop: -1
+    })
+  }
+
+  createHtmlUi () {
+    this.createCodeInput()
+    this.createMessageBox()
+  }
+
+  createCodeInput () {
+    const input = this.add.dom(0.5 * this.gameWidth(), 0.9167 * this.gameHeight()).createFromHTML(inputHtml)
+    input.setDepth(100)
+    input.setAlpha(0.6)
+    input.setVisible(false)
+    this.uiElements.input = input
+
+    document.getElementById('code_form').addEventListener('submit', (event) => {
+      event.preventDefault()
+      this.submitCode()
+    })
+
+    document.getElementById('hint1').addEventListener('pointerdown', this.showHint.bind(this, 0))
+    document.getElementById('hint2').addEventListener('pointerdown', this.showHint.bind(this, 1))
+    document.getElementById('hint3').addEventListener('pointerdown', this.showHint.bind(this, 2))
+  }
+
+  createMessageBox () {
+    const messageBox = this.add.dom(0.5 * this.gameWidth(), 0.5 * this.gameHeight()).createFromHTML(messageHtml)
+    this.uiElements.message = messageBox
+
+    const messageBoxStyle = messageBox.node.style
+    messageBoxStyle.width = (0.8 * this.gameWidth()) + 'px'
+    messageBox.setDepth(100)
+    messageBox.setVisible(false)
+
+    // Initially there is only one button. If more buttons are needed later, they are added dynamically
+    const messageBoxButton = document.getElementById('message_box_button_container').getElementsByTagName('button')[0]
+    this.addMessageBoxButtonHandler(messageBoxButton, 0)
+  }
+
+  addMessageBoxButtonHandler (buttonNode, index) {
+    buttonNode.addEventListener('pointerdown', (event) => {
+      event.preventDefault()
+      const actions = this.nextActions[index]
+      this.nextActions = null
+      this.processActions(actions)
     })
   }
 
@@ -225,5 +273,28 @@ export default class Game extends Phaser.Scene {
     // create_alternative_message_box(phaser_scene);
 
     // create_fade_rectangle(phaser_scene);
+  }
+
+  submitCode () {
+    console.log('Submitting from')
+    console.log(this)
+  }
+
+  showHint (hintIndex) {
+    console.log('Hint ' + hintIndex + ' from')
+    console.log(this)
+  }
+
+  objectClicked (objectName) {
+    console.log('Clicked ' + objectName)
+    // hide_message_if_open(phaser_scene);
+
+    // const cur_scene_id = phaser_scene.gameState.currentSceneId;
+    // const interactions = phaser_scene.gameDescription.scenes[cur_scene_id].interactive[object_name];
+    // process_actions(phaser_scene, interactions);
+  }
+
+  processActions (actions) {
+    console.log('TODO: process actions')
   }
 }
